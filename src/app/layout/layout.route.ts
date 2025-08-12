f6058ea1f5e08a1d5f61c017e9072228420f2357
Luwy Dyro <luwydyro@gmail.com>
2025-08-12 11:29:16 -0500

    Update Routes

--- src/app/layout/layout.route.ts
+++ src/app/layout/layout.route.ts
@@ -7,9 +7,7 @@ import { PricingTableFourComponent } from "../Pages/Pricing/pricing-table-four/p
 import { PricingTableThreeComponent } from "../Pages/Pricing/pricing-table-three/pricing-table-three.component";
 import { PricingTableTwoComponent } from "../Pages/Pricing/pricing-table-two/pricing-table-two.component";
 import { PricingTableOneComponent } from "../Pages/Pricing/pricing-table-one/pricing-table-one.component";
-import { LocationsComponent } from "../Pages/locations/locations.component";
 import { KnowledgebaseComponent } from "../Pages/support/knowledgebase/knowledgebase.component";
-import { FaqComponent } from "../Pages/support/faq/faq.component";
 import { ContactComponent } from "../Pages/support/contact/contact.component";
 import { AboutComponent } from "../Pages/company/about/about.component";
 import { AffliateComponent } from "../Pages/company/affliate/affliate.component";
@@ -32,9 +30,9 @@ export const MP_ROUTES: Routes = [
     { path: 'pricing-table-three', component: PricingTableThreeComponent },
     { path: 'pricing-table-four', component: PricingTableFourComponent },
 
-    { path: 'locations', component: LocationsComponent },
+    
     { path: 'knowledgebase', component: KnowledgebaseComponent },
-    { path: 'faq', component: FaqComponent },
+    
     { path: 'contact', component: ContactComponent },
     { path: 'about', component: AboutComponent },
     { path: 'affliate', component: AffliateComponent },
